27e8ee11c1f837245327732f8fb9d7170eb349d2
hyunmin-ooo <2201110357@office.kopo.ac.kr>
2025-04-09 11:30:39 +0900

    5장: 요청 처리 메소드에서 사용하는 @RequestParam, @PathVariable, @MatrixVariable 파라미터 유형

---
+++ src/main/java/kr/ac/kopo/ohm/chapter05test/controller/Example01Controller.java
@@ -0,0 +1,4 @@
+package kr.ac.kopo.ohm.chapter05test.controller;
+
+public class Example01Controller {
+}
---
+++ src/test/java/kr/ac/kopo/ohm/chapter05test/Chapter05TestApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.ohm.chapter05test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter05TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
